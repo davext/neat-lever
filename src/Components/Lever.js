1cbb0a48f2b656a7efda95aa2b2f4e7238c5eb9c
davext <dave_xt@hotmail.com>
2022-03-18 11:28:22 -0700

    Update Lever.js
    fix: change color to fix button

--- src/Components/Lever.js
+++ src/Components/Lever.js
@@ -68,7 +68,7 @@ export default function Lever(props) {
 
 
                         <a href={posting.hostedUrl + `?utm_source=${window.location}`} style={{ textDecoration: "none" }} target="_blank" rel="noreferrer"><div style={{
-                            backgroundColor: "#fff", color: "hotpink", padding: ".5rem 2rem .5rem 2rem", borderRadius: "1rem", fontWeight: "bold", marginBottom: "2rem",
+                            backgroundColor: "#fff", color: "#F79504", padding: ".5rem 2rem .5rem 2rem", borderRadius: "1rem", fontWeight: "bold", marginBottom: "2rem",
                             textTransform: "uppercase", textAlign: "center",
                         }}>Learn More</div></a>
 
